9255dc18a6b3dee5cb3bed5f67d010249b50fa0a
Praveer Pratap <Praveer Pratap>
2022-11-24 23:35:12 +0530

    24_11_2022

--- 02_Conditionals_&_Loop/03_Patterns.cpp
+++ 02_Conditionals_&_Loop/03_Patterns.cpp
@@ -1,9 +1,45 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     int i=1;
+//     while(i<=4){
+//         int j=1;
+//         while(j<=i){
+//             cout<<j;
+//             j=j+1;
+//         }
+//         i=i+1;
+//         cout<<endl;
+//     }
+
+//     return 0;
+// }
+
+
+
 #include<iostream>
 using namespace std;
+
 int main(){
+
+    int n;
+    cin>>n;
+    int i=1;
+    while(i<n){
+        
+        while(j<i){
+            cout<<j;
+            j=j+1;
+        }
+        cout<<endl;
+        i=i+1;
+    }
 
 
     return 0;
 }
+
 
 
--- 02_Conditionals_&_Loop/Assignment.cpp
+++ 02_Conditionals_&_Loop/Assignment.cpp
@@ -1,18 +1,67 @@
+// #include<iostream>
+// #include<cmath>
+// using namespace std;
+
+// int main(){
+//     int basicSalary;
+//     char grade;
+//     cout<<"Enter the Basic Salary and the Grade "<<endl;
+//     cin>>basicSalary>>grade;
+//     float totalSalary,hra,da,pf,allow;
+//     hra=(20.0/100)*basicSalary;
+//     da=(50.0/100)*basicSalary;
+//     pf=(11.0/100)*basicSalary;
+    
+//     if(grade=='A'){
+//         allow=1700;
+//     }else{
+//         if(grade=='B'){
+//             allow=1500;
+//         }else{
+//             allow=1300;
+//         }
+//     }
+//     totalSalary=round(hra+da+allow+basicSalary-pf);
+//     cout<<totalSalary;
+// }
+
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n,e=0,o=0;
+//     cin>>n;
+
+//     while(n>0){
+
+//         if((n%10)%2==0){
+//             e += n%10;
+//         }else{
+//             o += n%10;
+//         }
+//         n=n/10;
+//     }
+//     cout<<e<<" "<<o;
+//     return 0;
+// }
+
+
 #include<iostream>
 using namespace std;
-
 int main(){
-    int basicSalary;
-    char grade;
-    cout<<"Enter the Basic Salary and the Grade "<<endl;
-    cin>>basicSalary>>grade;
-    
 
-
-
-
-
-
-
-
+    int x,n,power=1;
+    cin>>x>>n;
+    while(n>0){
+        power=x*power;
+        n--;
+    }
+    cout<<power;
+    return 0;
 }
+
+
+
+
+
