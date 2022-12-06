4a71ee5ed1f20e0ca6fbb92c90e001e50fff8f08
Praveerpratap2803 <praveerpratap2803@gmail.com>
2022-12-06 19:04:20 +0530

    minor changes

--- 06_array/Assignment.cpp
+++ 06_array/Assignment.cpp
@@ -12,18 +12,18 @@ int main(){
 
 
 
-    int arr[7]={3,2,6,2,3,1,1};
-    int size=7,count=0;
-    int value,flag=0;
+    // int arr[7]={3,2,6,2,3,1,1};
+    // int size=7,count=0;
+    // int value,flag=0;
 
-    for(int i=0;i<size;i++){
-        for(int j=0;j<size;j++){
-            if(arr[i]==arr[j]&&i!=j){
-                count++;
-                value = arr[i];
-            }
-        }
-    }
+    // for(int i=0;i<size;i++){
+    //     for(int j=0;j<size;j++){
+    //         if(arr[i]==arr[j]&&i!=j){
+    //             count++;
+    //             value = arr[i];
+    //         }
+    //     }
+    // }
 
 
     // for(int i=0;i<size;i++){
